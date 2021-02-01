a901599ef60201cf912ac64f581676413ca833de
Sushrut <mail.sushrut@gmail.com>
2021-02-01 00:20:18 +0000

    Final optimization

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ActivisionTest
+
+Objective
+
+Implement a program to find all possible words of two letters or more created by a series of N lettered wheels with M letters per wheel, like a combination lock. 
+
--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@
 #include <thread>
 #include <mutex>
 
-#include "FileIO.h"
 
 using namespace std;
 
@@ -17,6 +16,9 @@ using namespace std;
 
 
 int noOfLines, noOfChars;
+int noOfMatches = 0;
+int wheelCombinationsdivided = 0;
+
 vector<string> wheelChars;
 vector<string> dictonary;
 vector<string> wheelCombinations;
@@ -24,38 +26,48 @@ vector<string> wheelCombinations;
 vector<string> finalStrings;
 
 
-int noOfMatches = 0;
 
 string fullCombinationList = "";
-string fullCombinationList2 = "";
-string fullCombinationList3 = "";
+string diagonalForwardList = "";
+string diagonalBackwardList = "";
 
 mutex mu;
 
 
+
+
 template <typename T>
-void remove_duplicates(std::vector<T>& vec)
+void RemoveDuplicates(std::vector<T>& vec)
 {
 	std::sort(vec.begin(), vec.end());
 	vec.erase(std::unique(vec.begin(), vec.end()), vec.end());
 }
 
+template<typename T>
+void PrintVector(vector<T>& vec)
+{
+	for (auto it = vec.begin(); it != vec.end(); it++)
+	{
+		cout << *it << endl;
+	}
+}
+
+
+
 
 bool ReadWheel(string wheelPath)
 {
-
 	std::ifstream wheelFile(wheelPath);
 	if (wheelFile.fail())
 	{
 		return false;
 	}
-
 	wheelFile >> noOfLines;
 	wheelFile >> noOfChars;
 
-
 	for (int i = 0; i < noOfLines; i++)
 	{
+		wheelChars.emplace_back("");
 		for (int j = 0; j < noOfChars; j++)
 		{
 			char currentChar;
@@ -90,30 +102,7 @@ bool ReadDictonary(string path)
 
 
 
-void CreateOffset(int lockedX, int xPointer)
-{
-
-	if (xPointer == noOfChars)
-	{
-		return;
-	}
-	else
-	{
-		string test = "";
-
-		test.push_back(wheelChars[0].at(lockedX));
-		for (int y = 1; y < noOfLines; y++)
-		{
-			test.push_back(wheelChars[y].at(xPointer));
-		}
-		wheelCombinations.emplace_back(test);
-
-		CreateOffset(lockedX, ++xPointer);
-	}
-}
-
-
-void CreateWheelCombinations(int lockedX, int lockedY, int yPointer, int xPointer)
+void CreateWheelCombinationsStr(int lockedX, int lockedY, int yPointer, int xPointer)
 {
 
 	if (yPointer == noOfLines)
@@ -122,7 +111,19 @@ void CreateWheelCombinations(int lockedX, int lockedY, int yPointer, int xPointe
 		xPointer++;
 	}
 
-	if (xPointer == noOfChars) 	 return;
+	if (xPointer == noOfChars)
+	{
+		xPointer = yPointer = 0;
+		lockedX++;
+	}
+
+	if (lockedX == noOfChars)
+	{
+		xPointer = yPointer = lockedX = 0;
+		lockedY++;
+	}
+	if (lockedY == noOfLines)
+		return;
 
 	if (yPointer == lockedY)
 	{
@@ -139,11 +140,11 @@ void CreateWheelCombinations(int lockedX, int lockedY, int yPointer, int xPointe
 		//mu.unlock();
 	}
 
-	CreateWheelCombinations(lockedX, lockedY, ++yPointer, xPointer);
+	CreateWheelCombinationsStr(lockedX, lockedY, ++yPointer, xPointer);
 }
 
 
-void CreateWheelCombinations2(int lockedX, int lockedY, int yPointer, int xPointer, int noOfIterations, int xJumpValue)
+void CreateWheelCombinationsFwdDiag(int lockedX, int lockedY, int yPointer, int xPointer, int noOfIterations, int xJumpValue)
 {
 
 	if (yPointer == noOfLines)
@@ -153,12 +154,8 @@ void CreateWheelCombinations2(int lockedX, int lockedY, int yPointer, int xPoint
 
 	if (xPointer >= noOfChars)
 	{
-		xPointer = 0;
+		xPointer = -1 + xJumpValue;
 	}
-	/*if (xPointer > noOfChars)
-	{
-		xPointer = noOfChars - xJumpValue - 1;
-	}*/
 
 	if (noOfIterations == 0)
 	{
@@ -173,39 +170,35 @@ void CreateWheelCombinations2(int lockedX, int lockedY, int yPointer, int xPoint
 	{
 		char ch = wheelChars[yPointer].at(lockedX);
 		//mu.lock();
-		fullCombinationList2.push_back(ch);
+		diagonalForwardList.push_back(ch);
 		//mu.unlock();
 	}
 	else
 	{
 		char ch = wheelChars[yPointer].at(xPointer);
 		//mu.lock();
-		fullCombinationList2.push_back(ch);
+		diagonalForwardList.push_back(ch);
 		//mu.unlock();
 	}
 
 	xPointer += xJumpValue;
 
-	CreateWheelCombinations2(lockedX, lockedY, ++yPointer, xPointer, --noOfIterations, xJumpValue);
+	CreateWheelCombinationsFwdDiag(lockedX, lockedY, ++yPointer, xPointer, --noOfIterations, xJumpValue);
 }
 
 
 
-void CreateWheelCombinations3(int lockedX, int lockedY, int yPointer, int xPointer, int noOfIterations, int xJumpValue)
+void CreateWheelCombinationsBckDiag(int lockedX, int lockedY, int yPointer, int xPointer, int noOfIterations, int xJumpValue)
 {
 	if (yPointer == noOfLines)
 	{
 		yPointer = 0;
 	}
 
-	if (xPointer <= 0)
+	if (xPointer < 0)
 	{
 		xPointer = noOfChars - 1;
 	}
-	/*if (xPointer < 0)
-	{
-		xPointer = xJumpValue - 1;
-	}*/
 
 	if (noOfIterations == 0)
 	{
@@ -220,20 +213,20 @@ void CreateWheelCombinations3(int lockedX, int lockedY, int yPointer, int xPoint
 	{
 		char ch = wheelChars[yPointer].at(lockedX);
 		//mu.lock();
-		fullCombinationList3.push_back(ch);
+		diagonalBackwardList.push_back(ch);
 		//mu.unlock();
 	}
 	else
 	{
 		char ch = wheelChars[yPointer].at(xPointer);
 		//mu.lock();
-		fullCombinationList3.push_back(ch);
+		diagonalBackwardList.push_back(ch);
 		//mu.unlock();
 	}
 
 	xPointer -= xJumpValue;
 
-	CreateWheelCombinations3(lockedX, lockedY, ++yPointer, xPointer, --noOfIterations, xJumpValue);
+	CreateWheelCombinationsBckDiag(lockedX, lockedY, ++yPointer, xPointer, --noOfIterations, xJumpValue);
 }
 
 
@@ -241,40 +234,32 @@ void CreateWheelCombinations3(int lockedX, int lockedY, int yPointer, int xPoint
 
 void CreateCombinationsStraight()
 {
-	for (int y = 0; y < noOfLines; y++)
-	{
-		for (int x = 0; x < noOfChars; x++)
-		{
-			CreateWheelCombinations(x, y, 0, 0);
-		}
-	}
+	CreateWheelCombinationsStr(0, 0, 0, 0);
 }
 
 
-void CreateCombinationsDiagonalFwd()
+void CreateCombinationsDiagonalForward()
 {
 	for (int y = 0; y < noOfLines; y++)
 	{
 		for (int x = 0; x < noOfChars; x++)
 		{
-			CreateWheelCombinations2(x, y, 0, 0, noOfLines * noOfChars, 1);
+			CreateWheelCombinationsFwdDiag(x, y, 0, 0, noOfLines * noOfChars, 1);
 		}
 	}
 }
-void CreateCombinationsDiagonalBckwd()
+void CreateCombinationsDiagonalBackword()
 {
 	for (int y = 0; y < noOfLines; y++)
 	{
 		for (int x = 0; x < noOfChars; x++)
 		{
-			CreateWheelCombinations3(x, y, 0, noOfChars - 1, noOfLines * noOfChars, noOfChars - 1);
+			CreateWheelCombinationsBckDiag(x, y, 0, noOfChars - 1, noOfLines * noOfChars, noOfChars - 1);
 		}
 	}
 }
 
-
-
-void FindInDictonary(int startIndex, int endIndex, vector<string> outFile)
+void FindInDictonary(int startIndex, int endIndex, vector<string>& outFile)
 {
 	for (int i = startIndex; i < endIndex; i++)
 	{
@@ -296,6 +281,9 @@ void FindInDictonary(int startIndex, int endIndex, vector<string> outFile)
 
 
 
+
+
+
 int main(int argc, char* argv[])
 {
 
@@ -306,43 +294,32 @@ int main(int argc, char* argv[])
 	}
 
 
-	bool wheelRead = ReadWheel(argv[1]);
-	if (!wheelRead)
+	if (!ReadWheel(argv[1]))
 	{
 		cout << "Wheel file not Read";
 		return 0;
 	}
 
-	bool DictoryRead = ReadDictonary(argv[2]);
-	if (!DictoryRead)
+	if (!ReadDictonary(argv[2]))
 	{
 		cout << "Dictory file not Read";
 		return 0;
 	}
 
+	//
+	// pattern Generation find all combinations 
+	//
 
+	thread patternThread1(&CreateCombinationsStraight);
+	thread patternThread2(&CreateCombinationsDiagonalForward);
+	thread patternThread3(&CreateCombinationsDiagonalBackword);
 
+	patternThread1.join();
+	patternThread2.join();
+	patternThread3.join();
 
-
-	//CreateCombinationsStraight();
-	//CreateCombinationsDiagonalFwd();
-	//CreateCombinationsDiagonalBckwd();
-	thread t11(&CreateCombinationsStraight);
-	thread t12(&CreateCombinationsDiagonalFwd);
-	thread t13(&CreateCombinationsDiagonalBckwd);
-
-	t11.join();
-	t12.join();
-	t13.join();
-
-	//string::append()
-
-	fullCombinationList.append(fullCombinationList2);
-	fullCombinationList.append(fullCombinationList3);
-
-
-
-
+	fullCombinationList.append(diagonalForwardList);
+	fullCombinationList.append(diagonalBackwardList);
 
 
 	int i = 0;
@@ -356,37 +333,34 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	remove_duplicates(wheelCombinations);
+	RemoveDuplicates(wheelCombinations);
 
+	//
+	// divide all combinations into 4 parts for multi threading 
+	//
 
-
-	/*for (int i = 0; i < wheelCombinations.size(); i++)
-	{
-		cout << endl << wheelCombinations[i];
-	}*/
-
-	//cout << endl << endl << endl;
-
-	int wheelCombinationsdivided = wheelCombinations.size() / 4;
+	wheelCombinationsdivided = wheelCombinations.size() / 4;
 
 	vector<string> finalStrings2;
 	vector<string> finalStrings3;
 	vector<string> finalStrings4;
 
 
-	auto start = chrono::high_resolution_clock::now();
+	//auto start = chrono::high_resolution_clock::now();
 
-	thread t1(FindInDictonary,                            0, wheelCombinationsdivided    , finalStrings);
-	thread t2(FindInDictonary, wheelCombinationsdivided    , wheelCombinationsdivided * 2, finalStrings2);
-	thread t3(FindInDictonary, wheelCombinationsdivided * 2, wheelCombinationsdivided * 3, finalStrings3);
-	thread t4(FindInDictonary, wheelCombinationsdivided * 2, wheelCombinations.size()    , finalStrings4);
+	thread t1(FindInDictonary, 0, wheelCombinationsdivided, std::ref(finalStrings));
+	thread t2(FindInDictonary, wheelCombinationsdivided, wheelCombinationsdivided * 2, std::ref(finalStrings2));
+	thread t3(FindInDictonary, wheelCombinationsdivided * 2, wheelCombinationsdivided * 3, std::ref(finalStrings3));
+	thread t4(FindInDictonary, wheelCombinationsdivided * 2, wheelCombinations.size(), std::ref(finalStrings4));
 
 	t1.join();
 	t2.join();
 	t3.join();
 	t4.join();
 
+	//FindInDictonary(0, wheelCombinations.size(), finalStrings);
 
+	// combine strings as mutex was slower compared to single thread
 	for (int i = 0; i < finalStrings2.size(); i++)
 	{
 		finalStrings.emplace_back(finalStrings2[i]);
@@ -400,24 +374,16 @@ int main(int argc, char* argv[])
 		finalStrings.emplace_back(finalStrings4[i]);
 	}
 
-	remove_duplicates(finalStrings);
+	RemoveDuplicates(finalStrings);
 
-	auto stop = chrono::high_resolution_clock::now();
 
-	auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-	cout << endl << "Matches found: " << duration.count();
+	//auto stop = chrono::high_resolution_clock::now();
+	//auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+	//cout << endl << "Matches found: " << duration.count();
 
-	
+	PrintVector(finalStrings);
+	cout << "Number of Matches Found: " << finalStrings.size();
+
 	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
